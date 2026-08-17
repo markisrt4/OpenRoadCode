f5226e307b0af6a9b7a6b35e0a4ba2272cfdfe6d
Mark Russell <markisrt4@gmail.com>
2026-08-17 13:20:33 -0400

    Add web frontend package

---
+++ frontends/web/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2026 Mark G. Russell
+# SPDX-License-Identifier: MIT
+
+"""Reusable browser frontend for OpenRoadCode."""
+
+from frontends.web.web_frontend import create_web_frontend
+
+__all__ = ["create_web_frontend"]
