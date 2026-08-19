b7c11a382f862dc55ec8e6890dcf01103219d52e
Mark Russell <markisrt4@gmail.com>
2026-08-19 00:18:30 -0400

    Export BLE GATT transport contract

--- hardware_io/bluetooth/__init__.py
+++ hardware_io/bluetooth/__init__.py
@@ -5,8 +5,16 @@ from hardware_io.bluetooth.ble_scanner import (
     BleDeviceInfo,
     BleScanner,
 )
+from hardware_io.bluetooth.ble_gatt_transport_if import (
+    BleGattState,
+    BleGattStatus,
+    BleGattTransportIf,
+)
 
 __all__ = [
     "BleDeviceInfo",
+    "BleGattState",
+    "BleGattStatus",
+    "BleGattTransportIf",
     "BleScanner",
 ]
