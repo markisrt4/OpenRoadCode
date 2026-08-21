1beb520380730acb854ef357dc8f5cb917e292c1
Mark Russell <markisrt4@gmail.com>
2026-08-21 14:54:08 -0400

    Publish Android browser GPS over ZeroMQ

--- apps/webUi/main.py
+++ apps/webUi/main.py
@@ -9,8 +9,26 @@ from apps.webUi.menu_catalog import create_web_ui_menu_pages
 from apps.webUi.navigation_session import WebNavigationSession
 from apps.webUi.spotify_session import WebSpotifySession
 from frontends.web import create_web_frontend
+from messaging.contracts.navigation import PositionStatePublisher
+from messaging.zeromq import ZeroMqPublisher
 
-navigation_session = WebNavigationSession()
+
+def _create_navigation_session() -> tuple[WebNavigationSession, ZeroMqPublisher | None]:
+    """Create WebUi navigation state and optional ZeroMQ position publication."""
+    enabled = os.environ.get("OPENROADCODE_ZMQ_POSITION_PUBLISH", "0") == "1"
+    if not enabled:
+        return WebNavigationSession(), None
+
+    endpoint = os.environ.get(
+        "OPENROADCODE_ZMQ_POSITION_ENDPOINT",
+        "tcp://0.0.0.0:5557",
+    )
+    publisher = ZeroMqPublisher(endpoint)
+    position_publisher = PositionStatePublisher(publisher)
+    return WebNavigationSession(position_sink=position_publisher.publish), publisher
+
+
+navigation_session, position_zmq_publisher = _create_navigation_session()
 spotify_session = WebSpotifySession()
 app = create_web_frontend(
     create_web_ui_menu_pages(),
