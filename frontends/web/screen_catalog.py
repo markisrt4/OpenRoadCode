44002b355f89edafc7eeefb7232da765f62a0c7a
Mark Russell <markisrt4@gmail.com>
2026-08-21 12:30:38 -0400

    Refresh visualizer renderer after drum write-order fix

--- frontends/web/screen_catalog.py
+++ frontends/web/screen_catalog.py
@@ -40,7 +40,7 @@ MUSIC_VISUALIZER_HTML = '''
 <script src="/web-assets/audio-analysis/spectrum_controls.js?v=3"></script>
 <script src="/web-assets/audio-analysis/percussion_classifier.js?v=4"></script>
 <script src="/web-assets/audio-analysis/kick_mode.js?v=2"></script>
-<script src="/web-assets/audio-analysis/music_visualizer_page.js?v=12"></script>
+<script src="/web-assets/audio-analysis/music_visualizer_page.js?v=13"></script>
 '''
 
 LIGHTING_HTML = '''
