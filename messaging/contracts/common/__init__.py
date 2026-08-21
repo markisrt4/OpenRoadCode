92f27f3719ea0fab0dc12af73bee8af62d30eccd
Mark Russell <markisrt4@gmail.com>
2026-08-21 13:32:50 -0400

    Add common messaging contract package

---
+++ messaging/contracts/common/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2026 Mark G. Russell
+# SPDX-License-Identifier: MIT
+
+"""Common public messaging contract types."""
+
+from .timestamp import encode_timestamp
+
+__all__ = ["encode_timestamp"]
