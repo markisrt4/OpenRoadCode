2618e999e464ce6ad99d1465c8264c0912d2cdda
Mark Russell <markisrt4@gmail.com>
2026-08-21 14:17:48 -0400

    Export ZeroMQ subscriber

--- messaging/zeromq/__init__.py
+++ messaging/zeromq/__init__.py
@@ -1,2 +1,7 @@
+# SPDX-FileCopyrightText: 2026 Mark G. Russell
+# SPDX-License-Identifier: MIT
+
 from .publisher import ZeroMqPublisher
-__all__ = ["ZeroMqPublisher"]
+from .subscriber import ZeroMqSubscriber
+
+__all__ = ["ZeroMqPublisher", "ZeroMqSubscriber"]
