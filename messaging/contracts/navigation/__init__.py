29907638515294a45b7ab2a27839823989abcbf3
Mark Russell <markisrt4@gmail.com>
2026-08-22 06:22:47 -0400

    Export navigation motion contract

--- messaging/contracts/navigation/__init__.py
+++ messaging/contracts/navigation/__init__.py
@@ -1,18 +1,16 @@
 # SPDX-FileCopyrightText: 2026 Mark G. Russell
 # SPDX-License-Identifier: MIT
 
+from .motion_state_codec import decode_motion_state, encode_motion_state
+from .motion_state_message import MotionStateData, MotionStateMessage
+from .motion_state_validator import validate_motion_state
 from .position_state_codec import decode_position_state, encode_position_state
 from .position_state_message import PositionStateData, PositionStateMessage
 from .position_state_publisher import PositionStatePublisher
 from .position_state_validator import validate_position_state
-from .topics import POSITION_STATE_TOPIC
+from .topics import MOTION_STATE_TOPIC, POSITION_STATE_TOPIC
 
 __all__ = [
-    "POSITION_STATE_TOPIC",
-    "PositionStateData",
-    "PositionStateMessage",
-    "PositionStatePublisher",
-    "decode_position_state",
-    "encode_position_state",
-    "validate_position_state",
+    "MOTION_STATE_TOPIC", "MotionStateData", "MotionStateMessage", "decode_motion_state", "encode_motion_state", "validate_motion_state",
+    "POSITION_STATE_TOPIC", "PositionStateData", "PositionStateMessage", "PositionStatePublisher", "decode_position_state", "encode_position_state", "validate_position_state",
 ]
