7f5ebc318741a204a36f61123cc57bcc6ae8d2b2
Mark Russell <markisrt4@gmail.com>
2026-08-22 16:26:05 -0400

    Add reusable SI unit conversions

---
+++ common/units/__init__.py
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: 2026 Mark G. Russell
+# SPDX-License-Identifier: MIT
+
+"""Reusable unit conversions for SI-normalized OpenRoadCode data."""
+
+from common.units.conversions import (
+    kelvin_to_celsius,
+    kelvin_to_fahrenheit,
+    kilograms_per_second_to_grams_per_second,
+    meters_per_second_to_kilometers_per_hour,
+    meters_per_second_to_miles_per_hour,
+    meters_to_feet,
+    pascals_to_kilopascals,
+    pascals_to_psi,
+    radians_per_second_to_rpm,
+    radians_to_degrees,
+)
+from common.units.unit_system import UnitSystem
+
+__all__ = [
+    "UnitSystem",
+    "kelvin_to_celsius",
+    "kelvin_to_fahrenheit",
+    "kilograms_per_second_to_grams_per_second",
+    "meters_per_second_to_kilometers_per_hour",
+    "meters_per_second_to_miles_per_hour",
+    "meters_to_feet",
+    "pascals_to_kilopascals",
+    "pascals_to_psi",
+    "radians_per_second_to_rpm",
+    "radians_to_degrees",
+]
