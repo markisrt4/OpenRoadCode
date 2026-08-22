fd7dcde8a57036abbc84ddbfed93bdb81e9d4166
Mark Russell <markisrt4@gmail.com>
2026-08-22 13:58:12 -0400

    Add attitude and IMU navigation topics

--- messaging/contracts/navigation/topics.py
+++ messaging/contracts/navigation/topics.py
@@ -3,3 +3,5 @@
 
 POSITION_STATE_TOPIC = "openroad.navigation.position"
 MOTION_STATE_TOPIC = "openroad.navigation.motion"
+ATTITUDE_STATE_TOPIC = "openroad.navigation.attitude"
+IMU_STATE_TOPIC = "openroad.navigation.imu"
