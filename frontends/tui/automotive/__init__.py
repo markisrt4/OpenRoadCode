f47ab604d279316b256bda3dae5e0c30706f4b04
Mark Russell <markisrt4@gmail.com>
2026-08-22 16:26:59 -0400

    Export shared unit system from TUI automotive package

--- frontends/tui/automotive/__init__.py
+++ frontends/tui/automotive/__init__.py
@@ -3,11 +3,11 @@
 
 """Reusable terminal automotive dashboard views."""
 
+from common.units import UnitSystem
 from frontends.tui.automotive.navigation_dashboard_view import (
     ACCELERATION_MODES,
     NavigationDashboardView,
 )
-from frontends.tui.automotive.unit_system import UnitSystem
 from frontends.tui.automotive.vehicle_dashboard_view import VehicleDashboardView
 
 __all__ = [
