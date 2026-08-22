6d282968f15b95798cd7e64036b6939a28ccdd6e
Mark Russell <markisrt4@gmail.com>
2026-08-22 09:11:35 -0400

    Export music lighting effect interfaces

--- controllers/music_lighting/__init__.py
+++ controllers/music_lighting/__init__.py
@@ -2,6 +2,16 @@
 # SPDX-License-Identifier: MIT
 
 from .music_lighting_controller import MusicLightingController
+from .music_lighting_output import MusicLightingOutput
+from .music_lighting_pattern_if import MusicLightingPatternIf
+from .music_lighting_patterns import create_default_music_lighting_patterns
 from .music_lighting_types import MusicLightingPatternId, MusicLightingState
 
-__all__ = ["MusicLightingController", "MusicLightingPatternId", "MusicLightingState"]
+__all__ = [
+    "MusicLightingController",
+    "MusicLightingOutput",
+    "MusicLightingPatternIf",
+    "MusicLightingPatternId",
+    "MusicLightingState",
+    "create_default_music_lighting_patterns",
+]
