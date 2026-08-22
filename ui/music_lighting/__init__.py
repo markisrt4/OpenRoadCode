981810ef445862f3f7596f5397047feaddffa608
Mark Russell <markisrt4@gmail.com>
2026-08-22 06:36:38 -0400

    Export music lighting UI contracts

---
+++ ui/music_lighting/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2026 Mark G. Russell
+# SPDX-License-Identifier: MIT
+
+from .music_lighting_request_handler_if import MusicLightingRequestHandlerIf
+from .music_lighting_ui_if import MusicLightingUiIf
+
+__all__ = ["MusicLightingRequestHandlerIf", "MusicLightingUiIf"]
