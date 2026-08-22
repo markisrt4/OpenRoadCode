9ae1ad49665937ff885fa20a89920fad0d5aef4e
Mark Russell <markisrt4@gmail.com>
2026-08-22 11:26:38 -0400

    Manage CarUI message dispatcher lifecycle

--- apps/carUi/car_ui_lifecycle.py
+++ apps/carUi/car_ui_lifecycle.py
@@ -7,6 +7,7 @@ from __future__ import annotations
 
 from apps.carUi.position_status_presenter import PositionStatusPresenter
 from apps.carUi.runtime.car_ui_input_runtime import CarUiInputRuntime
+from messaging.message_dispatcher import MessageDispatcher
 
 
 class CarUiLifecycle:
@@ -16,9 +17,11 @@ class CarUiLifecycle:
         self,
         position_presenter: PositionStatusPresenter,
         input_runtime: CarUiInputRuntime,
+        message_dispatcher: MessageDispatcher,
     ) -> None:
         self._position_presenter = position_presenter
         self._input_runtime = input_runtime
+        self._message_dispatcher = message_dispatcher
         self._started = False
 
     def start(self) -> None:
@@ -26,13 +29,17 @@ class CarUiLifecycle:
             return
         self._input_runtime.start()
         self._position_presenter.start()
+        self._message_dispatcher.start()
         self._started = True
 
     def stop(self) -> None:
-        # Both components are expected to tolerate stop-before-start. Keeping
-        # the calls unconditional also cleans up a partially completed start.
+        # Components are expected to tolerate stop-before-start. Keeping calls
+        # unconditional also cleans up a partially completed start.
         try:
             self._input_runtime.stop()
         finally:
-            self._position_presenter.stop()
-            self._started = False
+            try:
+                self._position_presenter.stop()
+            finally:
+                self._message_dispatcher.close()
+                self._started = False
