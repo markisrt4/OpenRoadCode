de7a2efc55cc2f8cfe6c66a4bcf39b475c8fc249
Mark Russell <markisrt4@gmail.com>
2026-08-23 09:48:48 -0400

    Remove Car UI local position source lifecycle

--- apps/carUi/car_ui_lifecycle.py
+++ apps/carUi/car_ui_lifecycle.py
@@ -5,7 +5,6 @@
 
 from __future__ import annotations
 
-from apps.carUi.position_status_presenter import PositionStatusPresenter
 from apps.carUi.runtime.car_ui_input_runtime import CarUiInputRuntime
 from messaging.message_dispatcher import MessageDispatcher
 
@@ -15,11 +14,9 @@ class CarUiLifecycle:
 
     def __init__(
         self,
-        position_presenter: PositionStatusPresenter,
         input_runtime: CarUiInputRuntime,
         message_dispatcher: MessageDispatcher,
     ) -> None:
-        self._position_presenter = position_presenter
         self._input_runtime = input_runtime
         self._message_dispatcher = message_dispatcher
         self._started = False
@@ -28,18 +25,12 @@ class CarUiLifecycle:
         if self._started:
             return
         self._input_runtime.start()
-        self._position_presenter.start()
         self._message_dispatcher.start()
         self._started = True
 
     def stop(self) -> None:
-        # Components are expected to tolerate stop-before-start. Keeping calls
-        # unconditional also cleans up a partially completed start.
         try:
             self._input_runtime.stop()
         finally:
-            try:
-                self._position_presenter.stop()
-            finally:
-                self._message_dispatcher.close()
-                self._started = False
+            self._message_dispatcher.close()
+            self._started = False
