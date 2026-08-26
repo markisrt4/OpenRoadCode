a0647fa7028400032006985b90304e2c282c9d97
Mark Russell <markisrt4@gmail.com>
2026-08-26 09:07:57 -0400

    Export Android hardware sensor devices

--- hardware_io/android/__init__.py
+++ hardware_io/android/__init__.py
@@ -3,12 +3,18 @@
 
 """Android hardware exposed by the OpenRoadCode Android Bridge."""
 
+from .barometer import AndroidBarometer, PressureSample
+from .imu import AndroidImu, ImuSample
 from .magnetometer import AndroidMagnetometer, MagnetometerSample
 from .sensor_bridge_client import AndroidSensorBridgeClient, AndroidImuSample
 
 __all__ = [
+    "AndroidBarometer",
+    "AndroidImu",
     "AndroidImuSample",
     "AndroidMagnetometer",
     "AndroidSensorBridgeClient",
+    "ImuSample",
     "MagnetometerSample",
+    "PressureSample",
 ]
