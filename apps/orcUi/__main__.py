bf8c928d2a744e3989d26dd851a1375c032f305e
Mark Russell <markisrt4@gmail.com>
2026-08-27 18:47:36 -0400

    Add ORC UI module entry point

---
+++ apps/orcUi/__main__.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2026 Mark G. Russell
+# SPDX-License-Identifier: MIT
+
+"""Module entry point for ``python -m apps.orcUi``."""
+
+from apps.orcUi.main import main
+
+
+if __name__ == "__main__":
+    main()
