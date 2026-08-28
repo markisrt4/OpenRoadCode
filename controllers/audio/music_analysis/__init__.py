8fc081b83db8a89b23a1f732292f8ca865899dc6
Mark Russell <markisrt4@gmail.com>
2026-08-28 19:33:03 -0400

    Export music analysis pipeline and fan-out

--- controllers/audio/music_analysis/__init__.py
+++ controllers/audio/music_analysis/__init__.py
@@ -3,11 +3,17 @@
 
 """Frontend-neutral music analysis API."""
 
+from .music_analysis_fanout import MusicAnalysisConsumer, MusicAnalysisFanout
+from .music_analysis_pipeline import MusicAnalysisCallback, MusicAnalysisPipeline
 from .music_analysis_types import MusicAnalysisState, PercussionActivity
 from .music_analyzer import MusicAnalyzer
 from .music_analyzer_if import MusicAnalyzerIf
 
 __all__ = [
+    "MusicAnalysisCallback",
+    "MusicAnalysisConsumer",
+    "MusicAnalysisFanout",
+    "MusicAnalysisPipeline",
     "MusicAnalysisState",
     "MusicAnalyzer",
     "MusicAnalyzerIf",
