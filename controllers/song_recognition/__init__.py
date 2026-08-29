da1753b6626bca2f7b49ee5958835ded48f441fc
Mark Russell <markisrt4@gmail.com>
2026-08-29 00:06:43 -0400

    Export ACRCloud song recognizer

--- controllers/song_recognition/__init__.py
+++ controllers/song_recognition/__init__.py
@@ -1,7 +1,14 @@
 # SPDX-FileCopyrightText: 2026 Mark G. Russell
 # SPDX-License-Identifier: MIT
 
+from .acrcloud_song_recognizer import AcrCloudConfig, AcrCloudSongRecognizer
 from .song_recognition_if import SongRecognitionIf, SongRecognitionResult
 from .unconfigured_song_recognizer import UnconfiguredSongRecognizer
 
-__all__ = ["SongRecognitionIf", "SongRecognitionResult", "UnconfiguredSongRecognizer"]
+__all__ = [
+    "AcrCloudConfig",
+    "AcrCloudSongRecognizer",
+    "SongRecognitionIf",
+    "SongRecognitionResult",
+    "UnconfiguredSongRecognizer",
+]
