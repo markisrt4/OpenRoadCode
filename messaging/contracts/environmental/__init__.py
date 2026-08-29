3282dcecfb8eba11a1d0ae79b6073df73f6ccc29
Mark Russell <markisrt4@gmail.com>
2026-08-29 00:52:19 -0400

    Export ambient light telemetry contract

--- messaging/contracts/environmental/__init__.py
+++ messaging/contracts/environmental/__init__.py
@@ -1,16 +1,25 @@
 # SPDX-FileCopyrightText: 2026 Mark G. Russell
 # SPDX-License-Identifier: MIT
 
+from .ambient_light_state_codec import decode_ambient_light_state, encode_ambient_light_state
+from .ambient_light_state_message import AmbientLightStateData, AmbientLightStateMessage
+from .ambient_light_state_validator import validate_ambient_light_state
 from .barometric_state_codec import decode_barometric_state, encode_barometric_state
 from .barometric_state_message import BarometricStateData, BarometricStateMessage
 from .barometric_state_validator import validate_barometric_state
-from .topics import BAROMETRIC_STATE_TOPIC
+from .topics import AMBIENT_LIGHT_STATE_TOPIC, BAROMETRIC_STATE_TOPIC
 
 __all__ = [
+    "AMBIENT_LIGHT_STATE_TOPIC",
     "BAROMETRIC_STATE_TOPIC",
+    "AmbientLightStateData",
+    "AmbientLightStateMessage",
     "BarometricStateData",
     "BarometricStateMessage",
+    "decode_ambient_light_state",
     "decode_barometric_state",
+    "encode_ambient_light_state",
     "encode_barometric_state",
+    "validate_ambient_light_state",
     "validate_barometric_state",
 ]
