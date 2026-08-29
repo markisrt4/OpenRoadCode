31b3ab428bbe10ccf483d68cd50a7395aff0cbb3
Mark Russell <markisrt4@gmail.com>
2026-08-29 01:29:15 -0400

    Export Android ambient light sensor

--- hardware_io/android/__init__.py
+++ hardware_io/android/__init__.py
@@ -3,12 +3,14 @@
 
 """Android hardware exposed by the OpenRoadCode Android Bridge."""
 
+from .ambient_light import AndroidAmbientLight
 from .barometer import AndroidBarometer, PressureSample
 from .imu import AndroidImu, ImuSample
 from .magnetometer import AndroidMagnetometer, MagnetometerSample
 from .sensor_bridge_client import AndroidSensorBridgeClient, AndroidImuSample
 
 __all__ = [
+    "AndroidAmbientLight",
     "AndroidBarometer",
     "AndroidImu",
     "AndroidImuSample",
