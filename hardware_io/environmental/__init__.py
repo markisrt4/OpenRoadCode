89bd19f60d80fcdf3b7a019f08c405075c02f87d
Mark Russell <markisrt4@gmail.com>
2026-08-29 01:00:38 -0400

    Export ambient light sensor interface

--- hardware_io/environmental/__init__.py
+++ hardware_io/environmental/__init__.py
@@ -1,14 +1,16 @@
 # SPDX-FileCopyrightText: 2026 Mark G. Russell
 # SPDX-License-Identifier: MIT
 
-"""Barometric pressure sensor interfaces and implementations."""
+"""Environmental sensor interfaces and implementations."""
 
+from .ambient_light_sensor_if import AmbientLightSensorIf
 from .barometric_sensor_if import BarometricSensorIf
 from .bmp3xx import Bmp3xx
 from .bmp388 import Bmp388
 from .bmp390 import Bmp390
 
 __all__ = [
+    "AmbientLightSensorIf",
     "BarometricSensorIf",
     "Bmp3xx",
     "Bmp388",
