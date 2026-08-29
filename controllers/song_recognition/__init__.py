ac08807f89bfc617a530607648a97623d3aa217b
Mark Russell <markisrt4@gmail.com>
2026-08-29 00:05:55 -0400

    Add song recognition package

---
+++ controllers/song_recognition/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2026 Mark G. Russell
+# SPDX-License-Identifier: MIT
+
+from .song_recognition_if import SongRecognitionIf, SongRecognitionResult
+from .unconfigured_song_recognizer import UnconfiguredSongRecognizer
+
+__all__ = ["SongRecognitionIf", "SongRecognitionResult", "UnconfiguredSongRecognizer"]
