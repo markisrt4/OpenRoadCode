25254cf2669647ee25f73644a0a10cbb502c3c65
Mark Russell <markisrt4@gmail.com>
2026-08-30 20:51:11 -0400

    Make serial ELM327 import optional

--- hardware_io/automotive/elm327/__init__.py
+++ hardware_io/automotive/elm327/__init__.py
@@ -1,13 +1,20 @@
 # SPDX-FileCopyrightText: 2026 Mark G. Russell
 # SPDX-License-Identifier: MIT
 
-from hardware_io.automotive.elm327.elm327_device import Elm327Device
 from hardware_io.automotive.elm327.elm327_errors import (
     Elm327CommandError,
     Elm327ConnectionError,
     Elm327Error,
 )
 from hardware_io.automotive.elm327.elm327_response import Elm327Response
+from hardware_io.automotive.elm327.elm327_tcp_device import Elm327TcpDevice
+
+try:
+    from hardware_io.automotive.elm327.elm327_device import Elm327Device
+except ModuleNotFoundError as exc:
+    if exc.name != "serial":
+        raise
+    Elm327Device = None  # type: ignore[assignment,misc]
 
 __all__ = [
     "Elm327CommandError",
@@ -15,5 +22,5 @@ __all__ = [
     "Elm327Device",
     "Elm327Error",
     "Elm327Response",
+    "Elm327TcpDevice",
 ]
-
