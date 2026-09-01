76bf28ede7b83323d5d02b9ce32b62c894e1f8d9
Mark Russell <markisrt4@gmail.com>
2026-09-01 17:40:43 -0400

    Add UI theme contracts

---
+++ ui/theme/__init__.py
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2026 Mark G. Russell
+# SPDX-License-Identifier: MIT
+
+"""Frontend-neutral UI theme contracts."""
+
+from .style_sheet import StyleSheet, load_style_sheet, load_ui_theme
+from .theme_mode import ThemeMode
+from .theme_ui_if import ThemeUiIf
+from .ui_theme import Color, UiTheme
+
+__all__ = [
+    "Color",
+    "StyleSheet",
+    "ThemeMode",
+    "ThemeUiIf",
+    "UiTheme",
+    "load_style_sheet",
+    "load_ui_theme",
+]
