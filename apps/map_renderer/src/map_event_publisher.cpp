e701b7ae89ec4f3b975a1de8434ba7c9aaf4908c
Mark Russell <markisrt4@gmail.com>
2026-08-31 22:05:18 -0400

    Include generic classification in POI events

--- apps/map_renderer/src/map_event_publisher.cpp
+++ apps/map_renderer/src/map_event_publisher.cpp
@@ -6,7 +6,6 @@
 #include <rapidjson/document.h>
 #include <rapidjson/stringbuffer.h>
 #include <rapidjson/writer.h>
-
 #include <utility>
 
 namespace { constexpr const char* kPoiSelectedTopic = "map.poi.selected"; }
@@ -20,22 +19,24 @@ MapEventPublisher::MapEventPublisher(std::string endpoint)
 void MapEventPublisher::publishPoiSelected(
     const std::string& name,
     const std::string& brand,
+    const std::string& sourceClass,
+    const std::string& sourceSubclass,
     double latitude,
-    double longitude
-)
+    double longitude)
 {
     rapidjson::Document document;
     document.SetObject();
     auto& allocator = document.GetAllocator();
     document.AddMember("name", rapidjson::Value(name.c_str(), allocator), allocator);
     document.AddMember("brand", rapidjson::Value(brand.c_str(), allocator), allocator);
+    document.AddMember("class", rapidjson::Value(sourceClass.c_str(), allocator), allocator);
+    document.AddMember("subclass", rapidjson::Value(sourceSubclass.c_str(), allocator), allocator);
     document.AddMember("latitude", latitude, allocator);
     document.AddMember("longitude", longitude, allocator);
 
     rapidjson::StringBuffer buffer;
     rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
     document.Accept(writer);
-
     socket.send(zmq::buffer(kPoiSelectedTopic), zmq::send_flags::sndmore);
     socket.send(zmq::buffer(buffer.GetString(), buffer.GetSize()), zmq::send_flags::none);
 }
