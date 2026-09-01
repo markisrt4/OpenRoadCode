80c3f6aa89fce25ca2973526ddc34f41d6dee0d2
Mark Russell <markisrt4@gmail.com>
2026-09-01 17:41:40 -0400

    Add theme controller package

---
+++ controllers/theme/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2026 Mark G. Russell
+# SPDX-License-Identifier: MIT
+
+"""Application theme state management."""
+
+from .theme_controller import ThemeController, ThemeListener
+
+__all__ = ["ThemeController", "ThemeListener"]
