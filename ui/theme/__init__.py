d9777cbf3d0484424a2c2bfd6ae83cb1fc568961
Mark Russell <markisrt4@gmail.com>
2026-09-01 21:36:15 -0400

    Export resolved theme bundle

--- ui/theme/__init__.py
+++ ui/theme/__init__.py
@@ -4,6 +4,7 @@
 """Frontend-neutral UI theme contracts."""
 
 from .style_sheet import StyleSheet, load_style_sheet, load_ui_theme
+from .theme_bundle import ThemeBundle
 from .theme_mode import ThemeMode
 from .theme_ui_if import ThemeUiIf
 from .ui_theme import Color, UiTheme
@@ -11,6 +12,7 @@ from .ui_theme import Color, UiTheme
 __all__ = [
     "Color",
     "StyleSheet",
+    "ThemeBundle",
     "ThemeMode",
     "ThemeUiIf",
     "UiTheme",
