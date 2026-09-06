379f171a1ba40a41bd11199a4755b2523edd96f2
Mark Russell <markisrt4@gmail.com>
2026-09-06 01:47:36 -0400

    Export media screen adapters

--- frontends/tk/media/__init__.py
+++ frontends/tk/media/__init__.py
@@ -3,12 +3,16 @@
 
 """Reusable Tk media screens and panels."""
 
+from frontends.tk.media.browser_media_screen import BrowserMediaScreen
+from frontends.tk.media.media_screen import MediaScreen
+from frontends.tk.media.netflix_panel import NetflixPanel
 from frontends.tk.media.spotify_playback_panel import SpotifyPlaybackPanel
 from frontends.tk.media.spotify_screen import SpotifyScreen
-from frontends.tk.media.netflix_panel import NetflixPanel
 from frontends.tk.media.youtube_panel import YouTubePanel
 
 __all__ = [
+    "BrowserMediaScreen",
+    "MediaScreen",
     "NetflixPanel",
     "SpotifyPlaybackPanel",
     "SpotifyScreen",
