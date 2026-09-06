94efe88b694c94c703edd4a763bd2a5b853658f5
Mark Russell <markisrt4@gmail.com>
2026-09-06 14:08:09 -0400

    Export shared media navigation bar

--- frontends/tk/media/__init__.py
+++ frontends/tk/media/__init__.py
@@ -4,6 +4,7 @@
 """Reusable Tk media screens and panels."""
 
 from frontends.tk.media.browser_media_screen import BrowserMediaScreen
+from frontends.tk.media.media_navigation_bar import MediaNavigationBar
 from frontends.tk.media.media_screen import MediaScreen
 from frontends.tk.media.netflix_panel import NetflixPanel
 from frontends.tk.media.spotify_playback_panel import SpotifyPlaybackPanel
@@ -12,6 +13,7 @@ from frontends.tk.media.youtube_panel import YouTubePanel
 
 __all__ = [
     "BrowserMediaScreen",
+    "MediaNavigationBar",
     "MediaScreen",
     "NetflixPanel",
     "SpotifyPlaybackPanel",
