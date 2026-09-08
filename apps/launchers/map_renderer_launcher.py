b8b67d23d8116e234cb55ec8491cfbfbc36fa738
Mark Russell <markisrt4@gmail.com>
2026-09-08 06:27:06 -0400

    Store map renderer log under XDG state

--- apps/launchers/map_renderer_launcher.py
+++ apps/launchers/map_renderer_launcher.py
@@ -11,6 +11,7 @@ import subprocess
 from pathlib import Path
 
 from apps.launchers.process_manager import find_matching_processes, terminate_process
+from common.xdg_paths import openroadcode_state_dir
 
 _MAP_RENDERER_PROCESS_PATTERN = r"(^|/)openroadcode-map-renderer([[:space:]]|$)"
 
@@ -26,8 +27,7 @@ class MapRendererLauncher:
     ) -> None:
         self._command = command
         self._log_file = Path(
-            log_file
-            or Path.home() / ".cache" / "openroadcode" / "map-renderer.log"
+            log_file or openroadcode_state_dir("map-renderer", "map-renderer.log")
         )
         self._process: subprocess.Popen[str] | None = None
 
