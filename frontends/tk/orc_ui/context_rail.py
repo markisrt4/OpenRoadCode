5f8bf124579f9c54901d879c8f9e04667b24854d
Mark Russell <markisrt4@gmail.com>
2026-09-10 11:03:11 -0400

    Fix Tk context rail frontend imports

--- frontends/tk/orc_ui/context_rail.py
+++ frontends/tk/orc_ui/context_rail.py
@@ -9,7 +9,7 @@ import tkinter as tk
 from collections.abc import Callable
 from dataclasses import dataclass
 
-from apps.orcUi.context_offroad_panel import ContextOffroadPanel
+from frontends.tk.orc_ui.context_offroad_panel import ContextOffroadPanel
 from apps.orcUi.navigation_presenter import AttitudePresentationState, PositionPresentationState
 from apps.orcUi.theme_runtime import theme_bundle as packaged_theme_bundle
 from apps.orcUi.vehicle_presenter import VehiclePresentationState
