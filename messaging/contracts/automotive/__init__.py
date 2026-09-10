1809e04c81e1c7760f832cc5be0395f07e610075
Mark Russell <markisrt4@gmail.com>
2026-09-10 17:11:04 -0400

    Export automotive trip messaging contract

--- messaging/contracts/automotive/__init__.py
+++ messaging/contracts/automotive/__init__.py
@@ -1,4 +1,9 @@
-from .topics import VEHICLE_STATE_TOPIC
+from .topics import TRIP_STATE_TOPIC, VEHICLE_STATE_TOPIC
+from .trip_state_codec import encode_trip_state
+from .trip_state_decoder import decode_trip_state
+from .trip_state_message import TripStateData, TripStateMessage
+from .trip_state_publisher import TripStatePublisher
+from .trip_state_validator import validate_trip_state
 from .vehicle_state_codec import encode_vehicle_state
 from .vehicle_state_decoder import decode_vehicle_state
 from .vehicle_state_message import VehicleStateData, VehicleStateMessage
@@ -6,11 +11,18 @@ from .vehicle_state_publisher import VehicleStatePublisher
 from .vehicle_state_validator import validate_vehicle_state
 
 __all__ = [
+    "TRIP_STATE_TOPIC",
     "VEHICLE_STATE_TOPIC",
+    "TripStateData",
+    "TripStateMessage",
+    "TripStatePublisher",
     "VehicleStateData",
     "VehicleStateMessage",
     "VehicleStatePublisher",
+    "decode_trip_state",
     "decode_vehicle_state",
+    "encode_trip_state",
     "encode_vehicle_state",
+    "validate_trip_state",
     "validate_vehicle_state",
 ]
