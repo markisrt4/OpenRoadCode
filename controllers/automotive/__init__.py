d83309f27a54899a60bcb4c46abdf753c39e7daa
Mark Russell <markisrt4@gmail.com>
2026-09-10 17:04:23 -0400

    Export automotive trip tracker

--- controllers/automotive/__init__.py
+++ controllers/automotive/__init__.py
@@ -8,6 +8,7 @@ from typing import Any
 
 from controllers.automotive.trip_if import TripIf
 from controllers.automotive.trip_state import TripState, TripStatus
+from controllers.automotive.trip_tracker import TripTracker
 from controllers.automotive.vehicle_state import VehicleState
 from controllers.automotive.vehicle_state_source_if import VehicleStateSourceIf
 
@@ -18,6 +19,7 @@ __all__ = [
     "TripIf",
     "TripState",
     "TripStatus",
+    "TripTracker",
     "VehicleState",
     "VehicleStateSourceIf",
 ]
