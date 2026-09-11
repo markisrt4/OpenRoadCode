7fe2ec3a0bde39c499425813e9319b01327faa1f
Mark Russell <markisrt4@gmail.com>
2026-09-11 14:26:09 -0400

    Add Pi performance diagnostics

--- controllers/system/__init__.py
+++ controllers/system/__init__.py
@@ -1,11 +1,16 @@
 # SPDX-FileCopyrightText: 2026 Mark G. Russell
 # SPDX-License-Identifier: MIT
 
-"""System lifecycle controllers."""
+"""System lifecycle and diagnostics controllers."""
 
+from controllers.system.system_diagnostics_controller import SystemDiagnosticsController
 from controllers.system.system_lifecycle_controller import (
     SystemLifecycleAction,
     SystemLifecycleController,
 )
 
-__all__ = ["SystemLifecycleAction", "SystemLifecycleController"]
+__all__ = [
+    "SystemDiagnosticsController",
+    "SystemLifecycleAction",
+    "SystemLifecycleController",
+]
