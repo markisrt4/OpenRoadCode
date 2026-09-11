29b62f0f2d97daa8ec7d2e0fe7de8738632ffe42
Mark Russell <markisrt4@gmail.com>
2026-09-10 23:08:17 -0400

    Preserve follow mode during manual zoom

--- controllers/map_renderer/map_request_handler.py
+++ controllers/map_renderer/map_request_handler.py
@@ -85,7 +85,6 @@ class MapRequestHandler(MapRequestHandlerIf):
 
     def request_zoom(self, zoom_level: float) -> None:
         self._zoom_level = zoom_level
-        self.request_follow(False)
         self._renderer.set_zoom(zoom_level)
 
     def request_bearing(self, bearing_rad: float) -> None:
