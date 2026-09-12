eb689110199fe5450574ae24a802b0567840c5ed
Mark Russell <markisrt4@gmail.com>
2026-09-12 08:33:31 -0400

    Export vehicle configuration types

--- controllers/automotive/__init__.py
+++ controllers/automotive/__init__.py
@@ -10,18 +10,24 @@ from controllers.automotive.automotive_telemetry_profile import AutomotiveTeleme
 from controllers.automotive.trip_if import TripIf
 from controllers.automotive.trip_state import TripState, TripStatus
 from controllers.automotive.trip_tracker import TripTracker
+from controllers.automotive.vehicle_configuration import (
+    EngineInductionType,
+    VehicleConfiguration,
+)
 from controllers.automotive.vehicle_state import VehicleState
 from controllers.automotive.vehicle_state_source_if import VehicleStateSourceIf
 
 __all__ = [
     "AutomotiveTelemetryProfile",
     "Elm327ObdAdapter",
+    "EngineInductionType",
     "Obd2Manager",
     "SimulatedVehicleStateSource",
     "TripIf",
     "TripState",
     "TripStatus",
     "TripTracker",
+    "VehicleConfiguration",
     "VehicleState",
     "VehicleStateSourceIf",
 ]
