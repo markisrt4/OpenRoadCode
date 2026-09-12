2abfbb80f49a534b0dd5ae643d0176cd95b805f6
Mark Russell <markisrt4@gmail.com>
2026-09-12 07:47:37 -0400

    Add automotive telemetry profile request topic

--- messaging/contracts/automotive/topics.py
+++ messaging/contracts/automotive/topics.py
@@ -1,2 +1,4 @@
 VEHICLE_STATE_TOPIC = "openroad.vehicle.state"
 TRIP_STATE_TOPIC = "openroad.vehicle.trip.state"
+
+AUTOMOTIVE_TELEMETRY_PROFILE_REQUEST_TOPIC = "openroad.vehicle.telemetry.profile.request"
