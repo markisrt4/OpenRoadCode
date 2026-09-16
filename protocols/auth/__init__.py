6286b0ff6710428e1f7b32a36622144ec0da8f25
Mark Russell <markisrt4@gmail.com>
2026-09-16 19:25:23 -0400

    Add shared authentication package

---
+++ protocols/auth/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2026 Mark G. Russell
+# SPDX-License-Identifier: MIT
+
+"""Shared authentication persistence primitives."""
+
+from .secure_json_store import SecureJsonStore
+
+__all__ = ["SecureJsonStore"]
