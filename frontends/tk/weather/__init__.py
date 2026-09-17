1c31a2bcbb91053383eba9a94adbd540e938d1cf
Mark Russell <markisrt4@gmail.com>
2026-09-16 22:26:10 -0400

    Export native orcUi weather screen

--- frontends/tk/weather/__init__.py
+++ frontends/tk/weather/__init__.py
@@ -3,7 +3,9 @@
 
 """Reusable Tk weather presentation components."""
 
+from frontends.tk.weather.orc_weather_panel import OrcWeatherPanel
 from frontends.tk.weather.weather_menu_panel import WeatherMenuPanel
 from frontends.tk.weather.weather_panel import WeatherPanel
+from frontends.tk.weather.weather_screen import WeatherScreen
 
-__all__ = ["WeatherMenuPanel", "WeatherPanel"]
+__all__ = ["OrcWeatherPanel", "WeatherMenuPanel", "WeatherPanel", "WeatherScreen"]
