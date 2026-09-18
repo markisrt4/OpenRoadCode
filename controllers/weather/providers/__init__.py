8b75e8386ca8f44386670a7912b07e2a7e1d3983
Mark Russell <markisrt4@gmail.com>
2026-09-18 10:33:19 -0400

    Export NWS weather alert provider

--- controllers/weather/providers/__init__.py
+++ controllers/weather/providers/__init__.py
@@ -3,6 +3,7 @@
 
 """Weather provider implementations."""
 
+from controllers.weather.providers.nws_weather_alert_provider import NwsWeatherAlertProvider
 from controllers.weather.providers.open_meteo_weather_provider import OpenMeteoWeatherProvider
 
-__all__ = ["OpenMeteoWeatherProvider"]
+__all__ = ["NwsWeatherAlertProvider", "OpenMeteoWeatherProvider"]
