500d39183969c7604c666a444bcf24acf8917cd3
Mark Russell <markisrt4@gmail.com>
2026-09-20 06:22:32 -0400

    Export RainViewer radar provider

--- controllers/weather/providers/__init__.py
+++ controllers/weather/providers/__init__.py
@@ -5,5 +5,6 @@
 
 from controllers.weather.providers.nws_weather_alert_provider import NwsWeatherAlertProvider
 from controllers.weather.providers.open_meteo_weather_provider import OpenMeteoWeatherProvider
+from controllers.weather.providers.rainviewer_radar_provider import RainViewerRadarProvider
 
-__all__ = ["NwsWeatherAlertProvider", "OpenMeteoWeatherProvider"]
+__all__ = ["NwsWeatherAlertProvider", "OpenMeteoWeatherProvider", "RainViewerRadarProvider"]
