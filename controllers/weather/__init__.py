852182c6142c637827a8fdc84139d7c2dd5ceb38
Mark Russell <markisrt4@gmail.com>
2026-09-21 05:40:04 -0400

    Expose injected radar provider

--- controllers/weather/__init__.py
+++ controllers/weather/__init__.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: MIT
 """Toolkit-independent weather domain and provider contracts."""
 from controllers.weather.gpsd_weather_location_provider import GpsdWeatherLocationProvider
-from controllers.weather.providers import NwsWeatherAlertProvider, OpenMeteoWeatherProvider, RainViewerRadarProvider
+from controllers.weather.providers import InjectedRadarProvider, NwsWeatherAlertProvider, OpenMeteoWeatherProvider, RainViewerRadarProvider
 from controllers.weather.weather_alert import WeatherAlert, WeatherAlertEvent, WeatherAlertOperation, WeatherAlertClearReason, WeatherAlertCertainty, WeatherAlertSeverity, WeatherAlertUrgency
 from controllers.weather.weather_controller import WeatherController
 from controllers.weather.weather_presenter import WeatherPresenter
@@ -12,4 +12,4 @@ from controllers.weather.radar_tile_service import RadarTileService
 from controllers.weather.radar_provider_if import RadarFrame, RadarProviderIf
 from controllers.weather.weather_provider_if import WeatherProviderIf
 from controllers.weather.weather_state import CurrentWeather, DailyForecast, HourlyForecast, WeatherCondition, WeatherLocation, WeatherSource, WeatherState
-__all__=["CurrentWeather","DailyForecast","GpsdWeatherLocationProvider","HourlyForecast","NwsWeatherAlertProvider","OpenMeteoWeatherProvider","RadarFrame","RadarPalette","RadarProviderIf","RadarTileService","RainViewerRadarProvider","WeatherAlert","WeatherAlertEvent","WeatherAlertOperation","WeatherAlertClearReason","WeatherAlertCertainty","WeatherAlertSeverity","WeatherAlertUrgency","WeatherCondition","WeatherController","WeatherLocation","WeatherPresenter","WeatherRadarController","WeatherProviderIf","WeatherSource","WeatherState"]
+__all__=["CurrentWeather","InjectedRadarProvider","DailyForecast","GpsdWeatherLocationProvider","HourlyForecast","NwsWeatherAlertProvider","OpenMeteoWeatherProvider","RadarFrame","RadarPalette","RadarProviderIf","RadarTileService","RainViewerRadarProvider","WeatherAlert","WeatherAlertEvent","WeatherAlertOperation","WeatherAlertClearReason","WeatherAlertCertainty","WeatherAlertSeverity","WeatherAlertUrgency","WeatherCondition","WeatherController","WeatherLocation","WeatherPresenter","WeatherRadarController","WeatherProviderIf","WeatherSource","WeatherState"]
