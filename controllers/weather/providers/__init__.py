bdc5a2fca35ff17595cd23633489d20ae381c42f
Mark Russell <markisrt4@gmail.com>
2026-09-21 08:01:16 -0400

    Fix escaped newlines in radar wiring

--- controllers/weather/providers/__init__.py
+++ controllers/weather/providers/__init__.py
@@ -3,7 +3,8 @@
 
 """Weather provider implementations."""
 
-from controllers.weather.providers.injected_radar_provider import InjectedRadarProvider\nfrom controllers.weather.providers.nws_weather_alert_provider import NwsWeatherAlertProvider
+from controllers.weather.providers.injected_radar_provider import InjectedRadarProvider
+from controllers.weather.providers.nws_weather_alert_provider import NwsWeatherAlertProvider
 from controllers.weather.providers.open_meteo_weather_provider import OpenMeteoWeatherProvider
 from controllers.weather.providers.rainviewer_radar_provider import RainViewerRadarProvider
 
