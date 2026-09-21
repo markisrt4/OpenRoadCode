a8afceb47663c01aad8bbad2fb8a6a1aa86a5c23
Mark Russell <markisrt4@gmail.com>
2026-09-21 05:39:57 -0400

    Export injected radar provider

--- controllers/weather/providers/__init__.py
+++ controllers/weather/providers/__init__.py
@@ -3,8 +3,8 @@
 
 """Weather provider implementations."""
 
-from controllers.weather.providers.nws_weather_alert_provider import NwsWeatherAlertProvider
+from controllers.weather.providers.injected_radar_provider import InjectedRadarProvider\nfrom controllers.weather.providers.nws_weather_alert_provider import NwsWeatherAlertProvider
 from controllers.weather.providers.open_meteo_weather_provider import OpenMeteoWeatherProvider
 from controllers.weather.providers.rainviewer_radar_provider import RainViewerRadarProvider
 
-__all__ = ["NwsWeatherAlertProvider", "OpenMeteoWeatherProvider", "RainViewerRadarProvider"]
+__all__ = ["InjectedRadarProvider", "NwsWeatherAlertProvider", "OpenMeteoWeatherProvider", "RainViewerRadarProvider"]
